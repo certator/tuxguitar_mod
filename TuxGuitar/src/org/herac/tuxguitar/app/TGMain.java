48e3549780745fd0cbe036b39aa532780ce82874
kos <kos@kos.local>
2012-11-20 20:26:52 +0400

    eclipse project

--- TuxGuitar/src/org/herac/tuxguitar/app/TGMain.java
+++ TuxGuitar/src/org/herac/tuxguitar/app/TGMain.java
@@ -1,8 +1,18 @@
 package org.herac.tuxguitar.app;
 
+import java.util.Map.Entry;
+
 public class TGMain {
 	
 	public static void main(String[] args){
+		
+		
+		for (Entry<Object, Object> entry : System.getProperties().entrySet())
+		{
+			
+			System.out.println("property: " + entry.getKey().toString() + " = " + entry.getValue().toString());
+		}
+		
 		TuxGuitar.instance().displayGUI(args);
 		System.exit(0);
 	}
